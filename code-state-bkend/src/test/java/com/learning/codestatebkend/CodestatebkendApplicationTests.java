f265795a4c6e8b9f581acc6c9e44958b5edb3563
Mohit Shrivastava <mohit.shrivastava@hansencx.com>
2023-01-27 19:04:27 +0530

    Unable to find a SpringBootConfiguration sol 1

--- code-state-bkend/src/test/java/com/learning/codestatebkend/CodestatebkendApplicationTests.java
+++ code-state-bkend/src/test/java/com/learning/codestatebkend/CodestatebkendApplicationTests.java
@@ -1,13 +1,8 @@
-package com.learning.kubernetesdemo;
+package com.learning.codestatebkend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CodestatebkendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
